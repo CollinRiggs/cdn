12408ffbdd7cf74b82088f996a2c06f263f7ee49
Collin Riggs <mail@collinriggs.com>
2023-05-03 00:29:08 -0230

    Initial commit

---
+++ main-ms/main.js
@@ -0,0 +1,44 @@
+import { credentials, loadPackageDefinition } from "@grpc/grpc-js";
+import { loadSync } from "@grpc/proto-loader";
+import express from 'express';
+
+const REST_PORT = 5000;
+
+const PROJECT_PROTO_PATH = "./protos/projects.proto";
+const projectPackageDefinition = loadSync(PROJECT_PROTO_PATH);
+const projectsProto = loadPackageDefinition(projectPackageDefinition);
+
+const projectsStub = new projectsProto.Projects('0.0.0.0:50051', credentials.createInsecure());
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/projects', (_, res) => {
+    projectsStub.all({id: 0}, (err, projectStubs) => {
+        if (err) {
+            res.status(500).send('Could not fetch projects');
+            return;
+        }
+        res.send(projectStubs);
+    });
+});
+
+app.get('/projects/:id', (req, res) => {
+    if (!req.params.id) {
+        res.status(400).send('Invalid project ID');
+        return;
+    }
+    projectsStub.get({id: req.params.id}, (err, project) => {
+        if (err) {
+            res.status(500).send('Could not fetch project');
+            return;
+        }
+        res.send(project);
+    });
+});
+
+app.listen(REST_PORT, () => {
+    console.log(`RESTful API listening on port ${REST_PORT}`);
+});
---
+++ projects-ms/main.js
@@ -0,0 +1,40 @@
+import { loadPackageDefinition, Server, ServerCredentials } from "@grpc/grpc-js";
+const PROTO_PATH = "./protos/projects.proto";
+import { loadSync } from "@grpc/proto-loader";
+
+import projectData from './projects.json' assert { type: 'json' };
+
+const packageDefinition = loadSync(PROTO_PATH);
+const projectsProto = loadPackageDefinition(packageDefinition);
+
+function getProject(call, callback) {
+    let project = projectData.find((project) => project.id === call.request.id);
+    if (project) callback(null, project);
+    else {
+        callback({
+            message: "Project not found",
+            code: grpc.status.INVALID_ARGUMENT
+        });
+    }
+}
+
+function getProjectStubs(_, callback) {
+    callback(null, {
+        projects: projectData.map(project => { return {
+            id: project.id,
+            title: project.title,
+            iconUrl: project.iconUrl,
+            tagline: project.tagline
+        }})
+    });
+}
+
+const server = new Server();
+server.addService(projectsProto.Projects.service, {
+    get: getProject,
+    all: getProjectStubs
+});
+server.bindAsync('0.0.0.0:50051', ServerCredentials.createInsecure(), () => {
+    server.start()
+    console.log("Project service running on 0.0.0.0:50051")
+});
